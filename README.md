6ffaade04268a2db4e4a835f60c982288aaf083c
bardicwarrior <thejenniferhaggerty@gmail.com>
2015-12-20 16:41:06 -0600

    Create config/ and README.md.

---
+++ README.md
@@ -0,0 +1,10 @@
+To run this program, you need a `config/index.js` file.
+This takes the format of :
+```
+{
+gmail: 'sender@email.com',
+password: 'base64encodedPassword',
+jobBoards: [/*rss feed of We Work Remotely,more to come*/],
+whiteList: [/*keywords to notify sender of job*/]
+}
+```
--- index.js
+++ index.js
@@ -1,14 +1,31 @@
+var config = require('./config');
+var jobBoards = config.jobBoards;
+var whiteList = config.whiteList;
 var request = require('request');
 var xml2js = require('xml2js');
 var parseString = require('xml2js').parseString;
+var base64 = require('base-64');
+var nodemailer = require('nodemailer');
+var transporter = nodemailer.createTransport({
+  service: 'Gmail',
+  auth: {
+  user: config.gmail,
+  pass: password
+  }
+});
 
-var jobBoards = [
-	"https://weworkremotely.com/categories/1-design/jobs.rss",
-	"https://weworkremotely.com/categories/2-programming/jobs.rss"
-];
-var whiteList = [
-  "junior","jr"
-];
+// if PASSWORD env.var is missing, exit the program
+if(!config) {
+  console.log("You need a config/ file!");
+  process.exit(1);
+}
+
+// if we get this far, PASSWORD is present. otherwise
+// we would have exited in the line above.
+var password = config.password;
+// we want to get the decoded password into a var
+password = base64.decode(password);
+
 
 // iterates(counts through) the jobBoards array
 for (var i = 0; i < jobBoards.length; i++) {
@@ -46,10 +63,17 @@ for (var i = 0; i < jobBoards.length; i++) {
                 }
                 if (!error && response.statusCode == 200) {
                   //body is html page for a matching job
-                  console.log(body);
+//                  console.log(body);
                   //TODO use jquery for apply link
                   //TODO follow apply link and apply
-                }
+                  console.log("Sending email...");
+                  transporter.sendMail({
+                      from: 'sender@address',
+                      to: config.gmail,
+                      subject: 'hello',
+                      text: 'hello world!'
+                  });
+                } 
               });
             }
           }
